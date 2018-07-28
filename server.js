95f547c5aea8602293c86f7bd0057fc46b69ec73
Vertig0ne <23177238+vertig0ne@users.noreply.github.com>
2018-07-28 23:09:21 +0100

    die if settings arent there rather then throw wierd errors

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ if (isDocker()) {
     }
 }
 
+if (!settings.tautulli.url || !settings.tautulli.apikey) die('Settings not configured. Tautulli unconfigured');
+if (!settings.sonarr.url || !settings.sonarr.apikey || !settings.sonarr.defaultProfile || !settings.sonarr.defaultRoot) die('Settings not configured. Sonarr unconfigured');
+if (!settings.radarr.url || !settings.radarr.apikey || !settings.radarr.defaultProfile || !settings.radarr.defaultRoot) die('Settings not configured. Sonarr unconfigured');
+if (!settings.plex.url) die('Settings not configured. Plex unconfigured');
+
+
 const plexService = require('./service/plexService');
 const requestService = require('./service/requestService');
 
