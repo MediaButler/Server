ad6cf0aa6aa839577e7d4eedba6dc155eb56c9cd
Vertig0ne <23177238+vertig0ne@users.noreply.github.com>
2018-07-30 22:26:33 +0100

    checking of settings

--- service/settingsService.js
+++ service/settingsService.js
@@ -3,15 +3,20 @@ const fs = require('fs');
 
 module.exports = class settingsService {
     constructor() {
-        if (isDocker()) {
-            this.settings = this._getSettings('/config/settings.json');
-        } else {
-            this.settings = this._getSettings('../settings.json');
-        }
+        if (isDocker()) this.settings = this._getSettings('/config/settings.json');
+        else this.settings = this._getSettings('../settings.json');
     }
 
     getSettings() {
         return this.settings;
+    }
+
+    _makeSettings(filePath) {
+        if (fs.existsSync(filePath)) throw new Error('Settings already exists');
+        const settings = require('../settings.default.json');
+        fs.writeFileSync(filePath, JSON.stringify(settings, null, 2), (err) => {
+            if (err) return console.error(err);
+        });
     }
 
     _getSettings(filePath) {
@@ -24,16 +29,8 @@ module.exports = class settingsService {
             return settings;
         } catch (err) {
             if (err.name == "SyntaxError") { console.log('There was an error loading your settings.json, please go back and verify the file is correct'); process.exit(1); }
-
-            if (err.message.startsWith("Cannot find module")) {
-                const settings = require('../settings.default.json');
-                fs.writeFileSync(filePath, JSON.stringify(settings, null, 2), (err) => {
-                    if (err) return console.error(err);
-                });
-                console.log('Settings file created. Please edit and restart.');
-                process.exit(1);
-            }
             console.error(err.message);
+            this._makeSettings(filePath);
         }
     }
 }
