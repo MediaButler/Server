9efdf96206a292f13bc864375826b7037d88746a
Vertig0ne <23177238+vertig0ne@users.noreply.github.com>
2018-07-31 03:00:49 +0100

    remove console log

--- api/hooks.js
+++ api/hooks.js
@@ -47,7 +47,6 @@ router.put('/radarr', (req, res) => {
 
 router.post('/plex', upload.single('thumb'), (req, res) => {
     try {
-        console.log(req);
         if (notificationService) notificationService.emit('plex',  JSON.parse(req.body.payload);
         return res.status(200).send('OK');
     } catch (err) {
